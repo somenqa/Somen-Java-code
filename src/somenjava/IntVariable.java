60fa6bbd279fa6d5c24bbb17c2ffd65b36f4c4d2
dipika.das <dipika.das@groupm.com>
2020-12-19 22:52:34 -0500

    Second Commit

--- src/somenjava/IntVariable.java
+++ src/somenjava/IntVariable.java
@@ -3,10 +3,10 @@ public class IntVariable {
 public static void main (String[]args){
 int Banana =10;
 int Orange =34;
-int Apple =120;
-int Total=Banana+Apple-Orange;
+int Apple =1202;
+int Totalnew=Banana+Apple-Orange;
 
-System.out.println(Total);
+System.out.println(Totalnew);
 }
 }
 
