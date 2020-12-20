f37e06f303b2a2aa7aa2cfda0ae963e1ba1a2ccd
dipika.das <dipika.das@groupm.com>
2020-12-20 10:29:57 -0500

    I added Boolean Class

---
+++ src/somenjava/BooleanType.java
@@ -0,0 +1,14 @@
+package somenjava;
+
+public class BooleanType {
+
+    static boolean bangladeshIndependent=true;
+    static boolean AmericaCloseToDhaka=false;
+
+    public static void main (String[]args){
+
+System.out.println(bangladeshIndependent);
+System.out.println(AmericaCloseToDhaka);
+
+}
+}
--- src/somenjava/IntVariable.java
+++ src/somenjava/IntVariable.java
@@ -4,9 +4,9 @@ public static void main (String[]args){
 int Banana =10;
 int Orange =34;
 int Apple =1202;
-int Totalnew=Banana+Apple-Orange;
+int Totalnewprice=Banana+Apple-Orange;
 
-System.out.println(Totalnew);
+System.out.println("Total new price: $"+ Totalnewprice);
 }
 }
 
