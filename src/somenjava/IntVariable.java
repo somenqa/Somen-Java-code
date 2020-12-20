52fdc046d258d4ea1b842bd68feef463ff9eca9c
dipika.das <dipika.das@groupm.com>
2020-12-19 22:39:19 -0500

    First Sample

---
+++ src/somenjava/IntVariable.java
@@ -0,0 +1,12 @@
+package somenjava;
+public class IntVariable {
+public static void main (String[]args){
+int Banana =10;
+int Orange =34;
+int Apple =120;
+int Total=Banana+Apple-Orange;
+
+System.out.println(Total);
+}
+}
+
