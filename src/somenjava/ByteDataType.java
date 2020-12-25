f5493beca00d3fcd7bd9c0919def6a1057819d50
dipika.das <dipika.das@groupm.com>
2020-12-24 20:01:32 -0500

    commit for ByteData Class

---
+++ src/somenjava/ByteDataType.java
@@ -0,0 +1,9 @@
+package somenjava;
+public class ByteDataType {
+public static void main (String[]args){
+        Byte Number;
+        Number=125;
+        System.out.println(Number);
+        }
+}
+
