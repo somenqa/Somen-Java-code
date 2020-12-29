363a708afb2b3d730c93c77ff81c039aa6f7575c
dipika.das <dipika.das@groupm.com>
2020-12-29 15:27:25 -0500

    DoubleDataType

--- src/somenjava/DoubleDataType.java
+++ src/somenjava/DoubleDataType.java
@@ -4,6 +4,7 @@ public class DoubleDataType {
 public static void main (String[]args){
 Double mango = 110.99;
 Double Pineapple = 124.99;
-System.out.println(mango+Pineapple);
+Double Coconut = 287.99;
+System.out.println(mango+Pineapple-Coconut);
     }
 }
