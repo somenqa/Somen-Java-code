f3fc1d7df422bf8f2cf85d7dc4798c7981c2ea94
dipika.das <dipika.das@groupm.com>
2020-12-29 13:17:02 -0500

    BooleanType

--- src/somenjava/BooleanType.java
+++ src/somenjava/BooleanType.java
@@ -1,12 +1,14 @@
 package somenjava;
-
 public class BooleanType {
 
-    static boolean bangladeshIndependent=true;
-    static boolean AmericaCloseToDhaka=false;
 
     public static void main (String[]args){
 
+        boolean bangladeshIndependent=true;
+        boolean AmericaCloseToDhaka=false;
+        int x=10;
+        int y=25;
+System.out.println(x>y);
 System.out.println(bangladeshIndependent);
 System.out.println(AmericaCloseToDhaka);
 
