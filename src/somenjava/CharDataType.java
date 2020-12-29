ac93a1fec9036a9270ffbe4b1feb9d63b743fbcd
dipika.das <dipika.das@groupm.com>
2020-12-29 15:22:03 -0500

    CharDataType

--- src/somenjava/CharDataType.java
+++ src/somenjava/CharDataType.java
@@ -3,7 +3,12 @@ package somenjava;
 public class CharDataType {
 public static void main(String[]args){
 
-    char a=78;char b=99;char c=108;
-    System.out.println(a+b+c);
+    char a=78;
+    char b=99;
+    char c=108;
+    char d = 'A';
+    System.out.println(a);
+    System.out.println(d);
 }
+
 }
