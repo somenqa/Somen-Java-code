b5e373ffabbbf5c02b4cc96c23147890a0cb209a
dipika.das <dipika.das@groupm.com>
2020-12-29 13:21:11 -0500

    Flaot Data

--- src/somenjava/FloatDataType.java
+++ src/somenjava/FloatDataType.java
@@ -1,4 +1,11 @@
 package somenjava;
 
 public class FloatDataType {
+public static void main (String[]argu){
+    float orange = 7.20f;
+    float apple = 3.78f;
+    System.out.println(apple);
+    System.out.println(orange);
+
+}
 }
