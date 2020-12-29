78a6b3241154b86f653543900a21250b733398bc
dipika.das <dipika.das@groupm.com>
2020-12-28 22:30:23 -0500

    newcode2

---
+++ src/somenjava/newcode2.java
@@ -0,0 +1,11 @@
+package somenjava;
+public class newcode2 {
+    public static void main(String[] args) {
+        String add = "from Today";
+        String add1 = "I will Learn";
+        String add2 = "Java Everyday";
+        System.out.println(add);
+        System.out.println(add1);
+        System.out.println(add2);
+    }
+}
