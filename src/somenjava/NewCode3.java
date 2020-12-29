4b607686d7cf518107df1dcacfbf750adfdf30cc
dipika.das <dipika.das@groupm.com>
2020-12-28 23:11:38 -0500

    I live in NJ

---
+++ src/somenjava/NewCode3.java
@@ -0,0 +1,12 @@
+package somenjava;
+
+public class NewCode3 {
+public static void main(String[]args){
+    String add ="From today";
+    String add1 =" I will study";
+    String add2 =" Java Everyday whole day";
+    System.out.println(add+add1+add2);
+}
+
+
+}
