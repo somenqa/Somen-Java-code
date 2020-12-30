65b1cd731f01f19b5511f325d3c87c1ebb1efd1a
dipika.das <dipika.das@groupm.com>
2020-12-30 11:56:46 -0500

    IfDataType

---
+++ src/somenjava/IfDataType.java
@@ -0,0 +1,12 @@
+package somenjava;
+
+    public class IfDataType{
+    public static void main(String[]args){
+
+     int CarPrice=9999;
+     int BusPrice=19999;
+     if (CarPrice>BusPrice);
+     System.out.println("CarPrice is Greater than BusPrice");
+}
+
+}
