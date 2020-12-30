9a411efa2d1002914188ad1ec8bd3d7ba29de61f
dipika.das <dipika.das@groupm.com>
2020-12-30 12:02:20 -0500

    IfElseDataType

---
+++ src/somenjava/IfElseDataType.java
@@ -0,0 +1,12 @@
+package somenjava;
+
+public class IfElseDataType {
+public static void main(String[]args){
+    int time = 10;
+    if (time < 12) {
+        System.out.println("Good day.");
+    } else {
+        System.out.println("Good evening.");
+    }
+}
+}
