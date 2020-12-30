54c497ee3ec71dbf9ffdf39ff4d99f6e87ab5ded
dipika.das <dipika.das@groupm.com>
2020-12-30 12:04:36 -0500

    IntVariable

--- src/somenjava/IntVariable.java
+++ src/somenjava/IntVariable.java
@@ -4,7 +4,7 @@ public static void main (String[]args){
 int Banana =10;
 int Orange =34;
 int Apple =1202;
-int Totalnewprice=Banana+Apple-Orange;
+int Totalnewprice=Banana+Apple+Orange;
 
 System.out.println("Total new price: $"+ Totalnewprice);
 }
