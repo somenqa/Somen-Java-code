3641e114682503f5b877a182abc6d734ef45908b
dipika.das <dipika.das@groupm.com>
2020-12-30 21:55:23 -0500

    Array

---
+++ src/somenjava/ArrayInJava.java
@@ -0,0 +1,11 @@
+package somenjava;
+
+public class ArrayInJava {
+    public static void main(String[] args) {
+        String[] fruits = {"Mango", "Banana", "Grapes", "Orange",};
+        int[] HouseNumberCount = {12, 34, 45, 67, 78};
+        System.out.println(fruits[0]);
+        System.out.println(HouseNumberCount[2]);
+    }
+
+}
