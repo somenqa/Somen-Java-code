0bef5add75300fb4d184c4a1bbc634f1e538446c
dipika.das <dipika.das@groupm.com>
2021-01-12 20:41:54 -0500

    NestedIfData

---
+++ src/somenjava/NestedIfData.java
@@ -0,0 +1,24 @@
+package somenjava;
+
+public class NestedIfData {
+ public static void main(String[]argu){
+     int i=50;
+
+     if(i==50){
+
+         if(i>60){
+         System.out.println("I is less then 60");
+         }
+         if (i>55){
+             System.out.println("I is lee then 55");
+         }
+         else{
+             System.out.println("I is ok");
+         }
+
+
+     }
+
+
+ }
+}
