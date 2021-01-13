f1f2a78811cd94d6250eff6f9b89a2baaa80e291
dipika.das <dipika.das@groupm.com>
2021-01-13 14:04:17 -0500

    WhileLoop Data

---
+++ src/somenjava/WhileLoopData.java
@@ -0,0 +1,12 @@
+package somenjava;
+
+public class WhileLoopData {
+public static void main(String[]args){
+
+    int i=100;
+    while(i<120){
+        System.out.println("result is "+i);
+       i++;
+    }
+}
+}
