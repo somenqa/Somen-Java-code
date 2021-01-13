44478381e7adb3c6ecc1d096e5699c968da75fce
dipika.das <dipika.das@groupm.com>
2021-01-13 14:02:57 -0500

    Do WhileLoop Data

---
+++ src/somenjava/DoWhileLoops.java
@@ -0,0 +1,13 @@
+package somenjava;
+
+public class DoWhileLoops {
+    public static void main(String[]args){
+
+        int b=35;
+        do{
+            System.out.println("Result is ok "+b);
+            b++;
+        }
+        while(b<40);
+    }
+}
