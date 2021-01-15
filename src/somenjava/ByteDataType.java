1b43d069596319a3dcffc789748cad1d0a209bf3
dipika.das <dipika.das@groupm.com>
2021-01-15 11:29:16 -0500

    ByteDataType

--- src/somenjava/ByteDataType.java
+++ src/somenjava/ByteDataType.java
@@ -1,8 +1,8 @@
 package somenjava;
 public class ByteDataType {
 public static void main (String[]args){
-        Byte Number;
-        Number=125;
+        byte Number=126;
+        byte HouseNumber=125;
         System.out.println(Number);
         }
 }
