bfd6781016648cab67ddfb6d87a00bab8f66e7b3
dipika.das <dipika.das@groupm.com>
2021-01-19 20:08:50 -0500

    InheritanceChild2

---
+++ src/secondpractice/InheritanceChild2.java
@@ -0,0 +1,14 @@
+package secondpractice;
+
+public class InheritanceChild2 extends InheritanceChild1{
+public static void main(String[]args) {
+
+    InheritanceChild2 test = new InheritanceChild2();
+    test.son1();
+    test.son2();
+
+
+}
+
+
+}
