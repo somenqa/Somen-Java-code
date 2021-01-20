59a6cadfcf65506b70f9db60663d4048e0e23a3c
dipika.das <dipika.das@groupm.com>
2021-01-19 20:09:16 -0500

    InheritanceParent

---
+++ src/secondpractice/InheritanceParent.java
@@ -0,0 +1,12 @@
+package secondpractice;
+
+public class InheritanceParent {
+
+    public void son1(){
+    System.out.println("Elder Son");
+
+    }
+    public void son2(){
+        System.out.println("Middle Son");
+    }
+}
