a6abdd0cd784070da915766acdf05ff7e95a8ffc
dipika.das <dipika.das@groupm.com>
2021-01-21 10:46:10 -0500

    Citi Bank Merhodoverriding Data

--- src/somenjava/CitiBank.java
+++ src/somenjava/CitiBank.java
@@ -1,14 +1,15 @@
 package somenjava;
 
-public class CitiBank extends MethodOveriding{
+    public class CitiBank extends MethodOveriding{
 
-    public static void main(String[] args){
+        public static void main(String[] args){
 
-        CitiBank test1=new CitiBank();
-        System.out.println(test1.getHomeLoan());
+            CitiBank test1=new CitiBank();
+            System.out.println(test1.getHomeLoan());
+        }
+
+        public double getHomeLoan(){
+            return 1.2;
+        }
     }
 
-    public double getHomeLoan(){
-        return 1.2;
-    }
-}
