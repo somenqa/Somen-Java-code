bc2e5b084304bb91bea7782d4d02de7961b0db35
dipika.das <dipika.das@groupm.com>
2021-01-21 10:37:07 -0500

    MethodOverriding

---
+++ src/somenjava/CitiBank.java
@@ -0,0 +1,14 @@
+package somenjava;
+
+public class CitiBank extends MethodOveriding{
+
+    public static void main(String[] args){
+
+        CitiBank test1=new CitiBank();
+        System.out.println(test1.getHomeLoan());
+    }
+
+    public double getHomeLoan(){
+        return 1.2;
+    }
+}
