1c0cdadd785e56ba4558eb98b88249f8d8b7093a
dipika.das <dipika.das@groupm.com>
2021-01-21 10:35:57 -0500

    Switch data

---
+++ src/somenjava/Switch.java
@@ -0,0 +1,27 @@
+package somenjava;
+
+public class Switch{
+
+    public static void main(String [] args){
+        int day=5;
+
+        switch (day){
+            case 1:
+            System.out.println("Monday");
+            break;
+            case 2:
+            System.out.println("Tuesday");
+            break;
+            case 3:
+            System.out.println("Wednessday");
+            break;
+            case 4:
+            System.out.println("Thrusday");
+            break;
+            case 5:
+            System.out.println("Friday");
+            break;
+        }
+
+    }
+}
