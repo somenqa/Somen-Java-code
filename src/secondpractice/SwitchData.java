ee35d6b008b001138f58d49a5aeecd07d6632db3
dipika.das <dipika.das@groupm.com>
2021-01-22 11:06:23 -0500

    Switch Data

---
+++ src/secondpractice/SwitchData.java
@@ -0,0 +1,52 @@
+package secondpractice;
+
+public class SwitchData {
+public static void main(String[]args){
+
+    int Month =3;
+
+    switch (Month){
+
+        case  1:
+            System.out.println("July");
+            break;
+
+        case 2:
+        System.out.println("March");
+            break;
+        case 3:
+        System.out.println("November");
+            break;
+        case 4:
+        System.out.println("December");
+            break;
+        case 5:
+        System.out.println("January");
+            break;
+        case 6:
+        System.out.println("June");
+            break;
+
+        case 7:
+        System.out.println("September");
+        break;
+
+        case 8:
+        System.out.println("August");
+        break;
+
+        case 9:
+        System.out.println("October");
+        break;
+        case 10:
+        System.out.println("April");
+
+    }
+
+}
+
+
+
+
+
+}
