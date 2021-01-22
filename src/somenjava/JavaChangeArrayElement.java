87910c1de44b8f0414006f89f2a0fae896623357
dipika.das <dipika.das@groupm.com>
2021-01-22 11:44:41 -0500

    JavaCharArrayElement

--- src/somenjava/JavaChangeArrayElement.java
+++ src/somenjava/JavaChangeArrayElement.java
@@ -1,9 +1,19 @@
 package somenjava;
 
     public class JavaChangeArrayElement {
-    public static void main(String[]argu){
-        String[]usastates = {"Newjersey","Ohio"};
+    public static void main(String[]argS){
+
+        String[] usastates = {"New jersey", "Maine",  "Ohio"};
+        String[] njtown = {"Newark", "Hillside", "trenton"};
+        String[] usaAirports = {"Newark", "JFK", "Florida", "Boston"};
+        String[] carbrand  = {"Honda", "Toyota", "Nissan", "BMW","Volvo", "Ford"};
+
+        System.out.println(carbrand[4]);
+        System.out.println(usaAirports[2]);
         System.out.println(usastates[0]);
+        System.out.println(njtown[1]);
+
 
     }
-}
+    }
+
