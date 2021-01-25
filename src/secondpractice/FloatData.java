98b814d477da0a0fe176daae2dd094c834a68acb
dipika.das <dipika.das@groupm.com>
2021-01-25 18:33:36 -0500

    Float Data

---
+++ src/secondpractice/FloatData.java
@@ -0,0 +1,16 @@
+package secondpractice;
+
+public class FloatData {
+public static void main(String[]args){
+
+float clothes = 199.99f;
+float gold = 299.00f;
+float bus = 799.99f;
+float diamond =100.00f;
+
+System.out.println(clothes);
+System.out.println(gold);
+System.out.println(bus-clothes);
+System.out.println(gold+diamond);
+}
+}
