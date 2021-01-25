2ed235a3d36595b33805e2fd4f8af5c21cef0eef
dipika.das <dipika.das@groupm.com>
2021-01-25 18:38:26 -0500

    New Code 5

---
+++ src/somenjava/NewCode5.java
@@ -0,0 +1,11 @@
+package somenjava;
+
+public class NewCode5{
+public static void main(String[]args){
+
+    String add= "from Today I will Learn Java Everyday";
+    System.out.println(add);
+
+
+}
+}
