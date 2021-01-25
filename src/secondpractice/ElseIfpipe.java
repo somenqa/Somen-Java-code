f31730aba0584b96a1f00c186a7ed52325c58099
dipika.das <dipika.das@groupm.com>
2021-01-25 18:34:51 -0500

    Else If Pipe

---
+++ src/secondpractice/ElseIfpipe.java
@@ -0,0 +1,16 @@
+package secondpractice;
+
+public class ElseIfpipe {
+public static void main(String[]args){
+
+    int NewYork = 11345;
+    int NewJersey = 07205;
+
+    if (NewYork ==13450 || NewJersey>07200){
+        System.out.println("New Jersey is good for living");
+    }
+    else {
+        System.out.println(" New York is busy city");
+    }
+}
+}
