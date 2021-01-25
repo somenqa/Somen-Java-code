84f47c3b7e7debafdb6f6aacbb7508ff79123c44
dipika.das <dipika.das@groupm.com>
2021-01-25 18:36:23 -0500

    Chase Bank

---
+++ src/somenjava/chaseBank.java
@@ -0,0 +1,13 @@
+package somenjava;
+
+public class chaseBank {
+    public static void main(String[] args){
+
+        chaseBank test1=new chaseBank();
+        System.out.println(test1.getHomeLoan());
+    }
+    public double getHomeLoan(){
+        return 1.2;
+    }
+}
+
