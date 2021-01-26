ece73431a61facd04e1135d629c4b361429b8e4a
dipika.das <dipika.das@groupm.com>
2021-01-26 13:14:43 -0500

    Method overriding

---
+++ src/somenjava/MethodOverriding.java
@@ -0,0 +1,18 @@
+package somenjava;
+
+public class MethodOverriding {
+
+    public static void test() {
+
+        int Apple=20;
+
+        if (Apple<10){
+            System.out.println(Apple);
+        }
+        else{
+            System.out.println("Wrong price");
+
+        }
+    }
+}
+
