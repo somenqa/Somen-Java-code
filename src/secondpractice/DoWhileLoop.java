9a2421bfc17c08ebb890b41aa2adcea6b2abdf5d
dipika.das <dipika.das@groupm.com>
2021-02-10 11:22:16 -0500

    DoWhileLoop

---
+++ src/secondpractice/DoWhileLoop.java
@@ -0,0 +1,18 @@
+package secondpractice;
+
+public class DoWhileLoop {
+   public static void main(String[]args){
+
+       int Apple = 120;
+
+       do {
+           System.out.println("Result is Correct " + Apple);
+           Apple++;
+
+       } while (Apple < 130);
+
+       }
+   }
+
+
+
