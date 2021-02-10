ab5abf610e18eac2425184b8580c72ba5bc8ec4d
dipika.das <dipika.das@groupm.com>
2021-02-09 21:32:52 -0500

    WhileLoopData

---
+++ src/secondpractice/WhileLoopData.java
@@ -0,0 +1,13 @@
+package secondpractice;
+
+public class WhileLoopData {
+ public static void main(String[]args){
+
+     int a =100;
+     while (a <150){
+
+         System.out.println("Result is " +a);
+         a++;
+     }
+ }
+}
