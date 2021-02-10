9e899530e547794a73f70d7bac127b523078411d
dipika.das <dipika.das@groupm.com>
2021-02-09 21:21:02 -0500

    Middle Number in ForLoop

---
+++ src/secondpractice/MiddleNmbInForLoop.java
@@ -0,0 +1,13 @@
+package secondpractice;
+
+public class MiddleNmbInForLoop {
+    public static void main(String[]args){
+
+        int a = 1;
+        int b = 100;
+        while (++a < --b);
+
+        System.out.println(a);
+
+    }
+   }
