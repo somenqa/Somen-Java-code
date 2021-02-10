86530007f6d5e9f0d2bbe2422a08acb0915c2d4e
dipika.das <dipika.das@groupm.com>
2021-02-09 21:49:37 -0500

    JavaChangeArrayElement

---
+++ src/secondpractice/JavaChangeArrayElementPractice.java
@@ -0,0 +1,16 @@
+package secondpractice;
+
+public class JavaChangeArrayElementPractice {
+public static void main(String[]args){
+
+    String[] UsaStates = {"Ohio", "Kansas", "Maine", "Alaska", "Georgia", "Texas"};
+    String[] UsaTvChannels = {"Cnn", "Cnbc", "Hbo", "AXN","Bloomberg",};
+    String[] UsaMobileCompany = {"T Mobile", "AT&T", "Verizon", "Lyca", "Metro", "Sprint",};
+    String[] UsaCar = {"Honda", "GMC", "Ford", "Toyota", "Nissan"};
+
+    System.out.println(UsaMobileCompany[2]);
+    System.out.println(UsaStates[1]);
+    System.out.println(UsaTvChannels[3]);
+    System.out.println(UsaCar[3]);
+}
+}
